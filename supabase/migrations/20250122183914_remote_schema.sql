e1ebe6868cf9d7e2f747317e34d9092c5dcf654c
Glenn Inizan <glenn@tractr.net>
2025-01-22 12:40:53 -0600

    WIP modelisation

---
+++ supabase/migrations/20250122183914_remote_schema.sql
@@ -0,0 +1,506 @@
+revoke delete on table "public"."StreamIAHistory" from "anon";
+
+revoke insert on table "public"."StreamIAHistory" from "anon";
+
+revoke references on table "public"."StreamIAHistory" from "anon";
+
+revoke select on table "public"."StreamIAHistory" from "anon";
+
+revoke trigger on table "public"."StreamIAHistory" from "anon";
+
+revoke truncate on table "public"."StreamIAHistory" from "anon";
+
+revoke update on table "public"."StreamIAHistory" from "anon";
+
+revoke delete on table "public"."StreamIAHistory" from "authenticated";
+
+revoke insert on table "public"."StreamIAHistory" from "authenticated";
+
+revoke references on table "public"."StreamIAHistory" from "authenticated";
+
+revoke select on table "public"."StreamIAHistory" from "authenticated";
+
+revoke trigger on table "public"."StreamIAHistory" from "authenticated";
+
+revoke truncate on table "public"."StreamIAHistory" from "authenticated";
+
+revoke update on table "public"."StreamIAHistory" from "authenticated";
+
+revoke delete on table "public"."StreamIAHistory" from "service_role";
+
+revoke insert on table "public"."StreamIAHistory" from "service_role";
+
+revoke references on table "public"."StreamIAHistory" from "service_role";
+
+revoke select on table "public"."StreamIAHistory" from "service_role";
+
+revoke trigger on table "public"."StreamIAHistory" from "service_role";
+
+revoke truncate on table "public"."StreamIAHistory" from "service_role";
+
+revoke update on table "public"."StreamIAHistory" from "service_role";
+
+revoke delete on table "public"."StreamIAMenuOCR" from "anon";
+
+revoke insert on table "public"."StreamIAMenuOCR" from "anon";
+
+revoke references on table "public"."StreamIAMenuOCR" from "anon";
+
+revoke select on table "public"."StreamIAMenuOCR" from "anon";
+
+revoke trigger on table "public"."StreamIAMenuOCR" from "anon";
+
+revoke truncate on table "public"."StreamIAMenuOCR" from "anon";
+
+revoke update on table "public"."StreamIAMenuOCR" from "anon";
+
+revoke delete on table "public"."StreamIAMenuOCR" from "authenticated";
+
+revoke insert on table "public"."StreamIAMenuOCR" from "authenticated";
+
+revoke references on table "public"."StreamIAMenuOCR" from "authenticated";
+
+revoke select on table "public"."StreamIAMenuOCR" from "authenticated";
+
+revoke trigger on table "public"."StreamIAMenuOCR" from "authenticated";
+
+revoke truncate on table "public"."StreamIAMenuOCR" from "authenticated";
+
+revoke update on table "public"."StreamIAMenuOCR" from "authenticated";
+
+revoke delete on table "public"."StreamIAMenuOCR" from "service_role";
+
+revoke insert on table "public"."StreamIAMenuOCR" from "service_role";
+
+revoke references on table "public"."StreamIAMenuOCR" from "service_role";
+
+revoke select on table "public"."StreamIAMenuOCR" from "service_role";
+
+revoke trigger on table "public"."StreamIAMenuOCR" from "service_role";
+
+revoke truncate on table "public"."StreamIAMenuOCR" from "service_role";
+
+revoke update on table "public"."StreamIAMenuOCR" from "service_role";
+
+revoke delete on table "public"."StreamIAMenuState" from "anon";
+
+revoke insert on table "public"."StreamIAMenuState" from "anon";
+
+revoke references on table "public"."StreamIAMenuState" from "anon";
+
+revoke select on table "public"."StreamIAMenuState" from "anon";
+
+revoke trigger on table "public"."StreamIAMenuState" from "anon";
+
+revoke truncate on table "public"."StreamIAMenuState" from "anon";
+
+revoke update on table "public"."StreamIAMenuState" from "anon";
+
+revoke delete on table "public"."StreamIAMenuState" from "authenticated";
+
+revoke insert on table "public"."StreamIAMenuState" from "authenticated";
+
+revoke references on table "public"."StreamIAMenuState" from "authenticated";
+
+revoke select on table "public"."StreamIAMenuState" from "authenticated";
+
+revoke trigger on table "public"."StreamIAMenuState" from "authenticated";
+
+revoke truncate on table "public"."StreamIAMenuState" from "authenticated";
+
+revoke update on table "public"."StreamIAMenuState" from "authenticated";
+
+revoke delete on table "public"."StreamIAMenuState" from "service_role";
+
+revoke insert on table "public"."StreamIAMenuState" from "service_role";
+
+revoke references on table "public"."StreamIAMenuState" from "service_role";
+
+revoke select on table "public"."StreamIAMenuState" from "service_role";
+
+revoke trigger on table "public"."StreamIAMenuState" from "service_role";
+
+revoke truncate on table "public"."StreamIAMenuState" from "service_role";
+
+revoke update on table "public"."StreamIAMenuState" from "service_role";
+
+revoke delete on table "public"."StreamIASellState" from "anon";
+
+revoke insert on table "public"."StreamIASellState" from "anon";
+
+revoke references on table "public"."StreamIASellState" from "anon";
+
+revoke select on table "public"."StreamIASellState" from "anon";
+
+revoke trigger on table "public"."StreamIASellState" from "anon";
+
+revoke truncate on table "public"."StreamIASellState" from "anon";
+
+revoke update on table "public"."StreamIASellState" from "anon";
+
+revoke delete on table "public"."StreamIASellState" from "authenticated";
+
+revoke insert on table "public"."StreamIASellState" from "authenticated";
+
+revoke references on table "public"."StreamIASellState" from "authenticated";
+
+revoke select on table "public"."StreamIASellState" from "authenticated";
+
+revoke trigger on table "public"."StreamIASellState" from "authenticated";
+
+revoke truncate on table "public"."StreamIASellState" from "authenticated";
+
+revoke update on table "public"."StreamIASellState" from "authenticated";
+
+revoke delete on table "public"."StreamIASellState" from "service_role";
+
+revoke insert on table "public"."StreamIASellState" from "service_role";
+
+revoke references on table "public"."StreamIASellState" from "service_role";
+
+revoke select on table "public"."StreamIASellState" from "service_role";
+
+revoke trigger on table "public"."StreamIASellState" from "service_role";
+
+revoke truncate on table "public"."StreamIASellState" from "service_role";
+
+revoke update on table "public"."StreamIASellState" from "service_role";
+
+revoke delete on table "public"."StreamIAState" from "anon";
+
+revoke insert on table "public"."StreamIAState" from "anon";
+
+revoke references on table "public"."StreamIAState" from "anon";
+
+revoke select on table "public"."StreamIAState" from "anon";
+
+revoke trigger on table "public"."StreamIAState" from "anon";
+
+revoke truncate on table "public"."StreamIAState" from "anon";
+
+revoke update on table "public"."StreamIAState" from "anon";
+
+revoke delete on table "public"."StreamIAState" from "authenticated";
+
+revoke insert on table "public"."StreamIAState" from "authenticated";
+
+revoke references on table "public"."StreamIAState" from "authenticated";
+
+revoke select on table "public"."StreamIAState" from "authenticated";
+
+revoke trigger on table "public"."StreamIAState" from "authenticated";
+
+revoke truncate on table "public"."StreamIAState" from "authenticated";
+
+revoke update on table "public"."StreamIAState" from "authenticated";
+
+revoke delete on table "public"."StreamIAState" from "service_role";
+
+revoke insert on table "public"."StreamIAState" from "service_role";
+
+revoke references on table "public"."StreamIAState" from "service_role";
+
+revoke select on table "public"."StreamIAState" from "service_role";
+
+revoke trigger on table "public"."StreamIAState" from "service_role";
+
+revoke truncate on table "public"."StreamIAState" from "service_role";
+
+revoke update on table "public"."StreamIAState" from "service_role";
+
+alter table "public"."StreamIAHistory" drop constraint "StreamIAHistory_state_fkey";
+
+alter table "public"."StreamIAMenuOCR" drop constraint "StreamIAMenuOCR_state_fkey";
+
+alter table "public"."StreamIAMenuState" drop constraint "MenuProcessState_menu_fkey";
+
+alter table "public"."StreamIAMenuState" drop constraint "StreamIAMenuState_state_fkey";
+
+alter table "public"."StreamIASellState" drop constraint "StreamIASellState_sell_fkey";
+
+alter table "public"."StreamIASellState" drop constraint "StreamIASellState_state_fkey";
+
+alter table "public"."StreamIAHistory" drop constraint "StreamIAHistory_pkey";
+
+alter table "public"."StreamIAMenuOCR" drop constraint "StreamIAMenuOCR_pkey";
+
+alter table "public"."StreamIAMenuState" drop constraint "MenuProcessState_pkey";
+
+alter table "public"."StreamIASellState" drop constraint "StreamIASellState_pkey";
+
+alter table "public"."StreamIAState" drop constraint "StreamIAProcessState_pkey";
+
+drop index if exists "public"."StreamIAMenuOCR_pkey";
+
+drop index if exists "public"."MenuProcessState_pkey";
+
+drop index if exists "public"."StreamIAHistory_pkey";
+
+drop index if exists "public"."StreamIAProcessState_pkey";
+
+drop index if exists "public"."StreamIASellState_pkey";
+
+drop table "public"."StreamIAHistory";
+
+drop table "public"."StreamIAMenuOCR";
+
+drop table "public"."StreamIAMenuState";
+
+drop table "public"."StreamIASellState";
+
+drop table "public"."StreamIAState";
+
+create table "public"."StreamIAMenuRunContext" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "updated_at" timestamp with time zone,
+    "menu" uuid not null,
+    "run" uuid not null
+);
+
+
+alter table "public"."StreamIAMenuRunContext" enable row level security;
+
+create table "public"."StreamIARun" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "updated_at" timestamp with time zone,
+    "status" "StreamIAProcessStatus" not null default 'created'::"StreamIAProcessStatus",
+    "error_message" text,
+    "current_step" "StreamIAStep",
+    "type" "StreamIAType" not null
+);
+
+
+alter table "public"."StreamIARun" enable row level security;
+
+create table "public"."StreamIARunStep" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "run" uuid not null,
+    "input" jsonb not null,
+    "output" jsonb,
+    "finished_at" timestamp with time zone,
+    "step" "StreamIAStep" not null,
+    "error_message" text
+);
+
+
+alter table "public"."StreamIARunStep" enable row level security;
+
+create table "public"."StreamIASellRunContext" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "updated_at" timestamp with time zone,
+    "sell" uuid not null,
+    "run" uuid not null
+);
+
+
+alter table "public"."StreamIASellRunContext" enable row level security;
+
+CREATE UNIQUE INDEX "MenuProcessState_pkey" ON public."StreamIAMenuRunContext" USING btree (id);
+
+CREATE UNIQUE INDEX "StreamIAHistory_pkey" ON public."StreamIARunStep" USING btree (id);
+
+CREATE UNIQUE INDEX "StreamIAProcessState_pkey" ON public."StreamIARun" USING btree (id);
+
+CREATE UNIQUE INDEX "StreamIASellState_pkey" ON public."StreamIASellRunContext" USING btree (id);
+
+alter table "public"."StreamIAMenuRunContext" add constraint "MenuProcessState_pkey" PRIMARY KEY using index "MenuProcessState_pkey";
+
+alter table "public"."StreamIARun" add constraint "StreamIAProcessState_pkey" PRIMARY KEY using index "StreamIAProcessState_pkey";
+
+alter table "public"."StreamIARunStep" add constraint "StreamIAHistory_pkey" PRIMARY KEY using index "StreamIAHistory_pkey";
+
+alter table "public"."StreamIASellRunContext" add constraint "StreamIASellState_pkey" PRIMARY KEY using index "StreamIASellState_pkey";
+
+alter table "public"."StreamIAMenuRunContext" add constraint "MenuProcessState_menu_fkey" FOREIGN KEY (menu) REFERENCES "Menu"(id) ON DELETE CASCADE not valid;
+
+alter table "public"."StreamIAMenuRunContext" validate constraint "MenuProcessState_menu_fkey";
+
+alter table "public"."StreamIAMenuRunContext" add constraint "StreamIAMenuState_state_fkey" FOREIGN KEY (run) REFERENCES "StreamIARun"(id) ON DELETE CASCADE not valid;
+
+alter table "public"."StreamIAMenuRunContext" validate constraint "StreamIAMenuState_state_fkey";
+
+alter table "public"."StreamIARunStep" add constraint "StreamIAHistory_state_fkey" FOREIGN KEY (run) REFERENCES "StreamIARun"(id) ON DELETE CASCADE not valid;
+
+alter table "public"."StreamIARunStep" validate constraint "StreamIAHistory_state_fkey";
+
+alter table "public"."StreamIASellRunContext" add constraint "StreamIASellState_sell_fkey" FOREIGN KEY (sell) REFERENCES "Sell"(id) ON DELETE CASCADE not valid;
+
+alter table "public"."StreamIASellRunContext" validate constraint "StreamIASellState_sell_fkey";
+
+alter table "public"."StreamIASellRunContext" add constraint "StreamIASellState_state_fkey" FOREIGN KEY (run) REFERENCES "StreamIARun"(id) ON DELETE CASCADE not valid;
+
+alter table "public"."StreamIASellRunContext" validate constraint "StreamIASellState_state_fkey";
+
+grant delete on table "public"."StreamIAMenuRunContext" to "anon";
+
+grant insert on table "public"."StreamIAMenuRunContext" to "anon";
+
+grant references on table "public"."StreamIAMenuRunContext" to "anon";
+
+grant select on table "public"."StreamIAMenuRunContext" to "anon";
+
+grant trigger on table "public"."StreamIAMenuRunContext" to "anon";
+
+grant truncate on table "public"."StreamIAMenuRunContext" to "anon";
+
+grant update on table "public"."StreamIAMenuRunContext" to "anon";
+
+grant delete on table "public"."StreamIAMenuRunContext" to "authenticated";
+
+grant insert on table "public"."StreamIAMenuRunContext" to "authenticated";
+
+grant references on table "public"."StreamIAMenuRunContext" to "authenticated";
+
+grant select on table "public"."StreamIAMenuRunContext" to "authenticated";
+
+grant trigger on table "public"."StreamIAMenuRunContext" to "authenticated";
+
+grant truncate on table "public"."StreamIAMenuRunContext" to "authenticated";
+
+grant update on table "public"."StreamIAMenuRunContext" to "authenticated";
+
+grant delete on table "public"."StreamIAMenuRunContext" to "service_role";
+
+grant insert on table "public"."StreamIAMenuRunContext" to "service_role";
+
+grant references on table "public"."StreamIAMenuRunContext" to "service_role";
+
+grant select on table "public"."StreamIAMenuRunContext" to "service_role";
+
+grant trigger on table "public"."StreamIAMenuRunContext" to "service_role";
+
+grant truncate on table "public"."StreamIAMenuRunContext" to "service_role";
+
+grant update on table "public"."StreamIAMenuRunContext" to "service_role";
+
+grant delete on table "public"."StreamIARun" to "anon";
+
+grant insert on table "public"."StreamIARun" to "anon";
+
+grant references on table "public"."StreamIARun" to "anon";
+
+grant select on table "public"."StreamIARun" to "anon";
+
+grant trigger on table "public"."StreamIARun" to "anon";
+
+grant truncate on table "public"."StreamIARun" to "anon";
+
+grant update on table "public"."StreamIARun" to "anon";
+
+grant delete on table "public"."StreamIARun" to "authenticated";
+
+grant insert on table "public"."StreamIARun" to "authenticated";
+
+grant references on table "public"."StreamIARun" to "authenticated";
+
+grant select on table "public"."StreamIARun" to "authenticated";
+
+grant trigger on table "public"."StreamIARun" to "authenticated";
+
+grant truncate on table "public"."StreamIARun" to "authenticated";
+
+grant update on table "public"."StreamIARun" to "authenticated";
+
+grant delete on table "public"."StreamIARun" to "service_role";
+
+grant insert on table "public"."StreamIARun" to "service_role";
+
+grant references on table "public"."StreamIARun" to "service_role";
+
+grant select on table "public"."StreamIARun" to "service_role";
+
+grant trigger on table "public"."StreamIARun" to "service_role";
+
+grant truncate on table "public"."StreamIARun" to "service_role";
+
+grant update on table "public"."StreamIARun" to "service_role";
+
+grant delete on table "public"."StreamIARunStep" to "anon";
+
+grant insert on table "public"."StreamIARunStep" to "anon";
+
+grant references on table "public"."StreamIARunStep" to "anon";
+
+grant select on table "public"."StreamIARunStep" to "anon";
+
+grant trigger on table "public"."StreamIARunStep" to "anon";
+
+grant truncate on table "public"."StreamIARunStep" to "anon";
+
+grant update on table "public"."StreamIARunStep" to "anon";
+
+grant delete on table "public"."StreamIARunStep" to "authenticated";
+
+grant insert on table "public"."StreamIARunStep" to "authenticated";
+
+grant references on table "public"."StreamIARunStep" to "authenticated";
+
+grant select on table "public"."StreamIARunStep" to "authenticated";
+
+grant trigger on table "public"."StreamIARunStep" to "authenticated";
+
+grant truncate on table "public"."StreamIARunStep" to "authenticated";
+
+grant update on table "public"."StreamIARunStep" to "authenticated";
+
+grant delete on table "public"."StreamIARunStep" to "service_role";
+
+grant insert on table "public"."StreamIARunStep" to "service_role";
+
+grant references on table "public"."StreamIARunStep" to "service_role";
+
+grant select on table "public"."StreamIARunStep" to "service_role";
+
+grant trigger on table "public"."StreamIARunStep" to "service_role";
+
+grant truncate on table "public"."StreamIARunStep" to "service_role";
+
+grant update on table "public"."StreamIARunStep" to "service_role";
+
+grant delete on table "public"."StreamIASellRunContext" to "anon";
+
+grant insert on table "public"."StreamIASellRunContext" to "anon";
+
+grant references on table "public"."StreamIASellRunContext" to "anon";
+
+grant select on table "public"."StreamIASellRunContext" to "anon";
+
+grant trigger on table "public"."StreamIASellRunContext" to "anon";
+
+grant truncate on table "public"."StreamIASellRunContext" to "anon";
+
+grant update on table "public"."StreamIASellRunContext" to "anon";
+
+grant delete on table "public"."StreamIASellRunContext" to "authenticated";
+
+grant insert on table "public"."StreamIASellRunContext" to "authenticated";
+
+grant references on table "public"."StreamIASellRunContext" to "authenticated";
+
+grant select on table "public"."StreamIASellRunContext" to "authenticated";
+
+grant trigger on table "public"."StreamIASellRunContext" to "authenticated";
+
+grant truncate on table "public"."StreamIASellRunContext" to "authenticated";
+
+grant update on table "public"."StreamIASellRunContext" to "authenticated";
+
+grant delete on table "public"."StreamIASellRunContext" to "service_role";
+
+grant insert on table "public"."StreamIASellRunContext" to "service_role";
+
+grant references on table "public"."StreamIASellRunContext" to "service_role";
+
+grant select on table "public"."StreamIASellRunContext" to "service_role";
+
+grant trigger on table "public"."StreamIASellRunContext" to "service_role";
+
+grant truncate on table "public"."StreamIASellRunContext" to "service_role";
+
+grant update on table "public"."StreamIASellRunContext" to "service_role";
+
+
