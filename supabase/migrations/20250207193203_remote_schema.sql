0404210450f94e664a8c87f564b230849b913b55
Glenn Inizan <glenn@tractr.net>
2025-02-07 13:33:02 -0600

    feat: add profile migration

---
+++ supabase/migrations/20250207193203_remote_schema.sql
@@ -0,0 +1,84 @@
+create table "public"."profiles" (
+    "id" uuid not null,
+    "first_name" text,
+    "last_name" text
+);
+
+
+alter table "public"."profiles" enable row level security;
+
+CREATE UNIQUE INDEX profiles_pkey ON public.profiles USING btree (id);
+
+alter table "public"."profiles" add constraint "profiles_pkey" PRIMARY KEY using index "profiles_pkey";
+
+alter table "public"."profiles" add constraint "profiles_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON DELETE CASCADE not valid;
+
+alter table "public"."profiles" validate constraint "profiles_id_fkey";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+ SET search_path TO ''
+AS $function$
+begin
+  insert into public.profiles (id, first_name, last_name)
+  values (new.id, new.raw_user_meta_data ->> 'first_name', new.raw_user_meta_data ->> 'last_name');
+  return new;
+end;
+$function$
+;
+
+grant delete on table "public"."profiles" to "anon";
+
+grant insert on table "public"."profiles" to "anon";
+
+grant references on table "public"."profiles" to "anon";
+
+grant select on table "public"."profiles" to "anon";
+
+grant trigger on table "public"."profiles" to "anon";
+
+grant truncate on table "public"."profiles" to "anon";
+
+grant update on table "public"."profiles" to "anon";
+
+grant delete on table "public"."profiles" to "authenticated";
+
+grant insert on table "public"."profiles" to "authenticated";
+
+grant references on table "public"."profiles" to "authenticated";
+
+grant select on table "public"."profiles" to "authenticated";
+
+grant trigger on table "public"."profiles" to "authenticated";
+
+grant truncate on table "public"."profiles" to "authenticated";
+
+grant update on table "public"."profiles" to "authenticated";
+
+grant delete on table "public"."profiles" to "service_role";
+
+grant insert on table "public"."profiles" to "service_role";
+
+grant references on table "public"."profiles" to "service_role";
+
+grant select on table "public"."profiles" to "service_role";
+
+grant trigger on table "public"."profiles" to "service_role";
+
+grant truncate on table "public"."profiles" to "service_role";
+
+grant update on table "public"."profiles" to "service_role";
+
+create policy "Enable read access for all users"
+on "public"."profiles"
+as permissive
+for select
+to authenticated
+using (true);
+
+
+
